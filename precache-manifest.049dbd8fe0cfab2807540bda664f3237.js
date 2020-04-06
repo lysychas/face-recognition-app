94f00da6b851f46ae63b7b31858f61e20b6905ed
Lukas Lysychas <sayshi108@gmail.com>
2020-04-06 22:58:32 +0300

    Updates

---
+++ precache-manifest.049dbd8fe0cfab2807540bda664f3237.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fda29ffe00592cdbca13cc4c180dce3c",
+    "url": "/face-recognition-app/index.html"
+  },
+  {
+    "revision": "8307e871c057266ce06b",
+    "url": "/face-recognition-app/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "b05eb84ac365005cf281",
+    "url": "/face-recognition-app/static/css/main.35c77881.chunk.css"
+  },
+  {
+    "revision": "8307e871c057266ce06b",
+    "url": "/face-recognition-app/static/js/2.4f583dac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/face-recognition-app/static/js/2.4f583dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b05eb84ac365005cf281",
+    "url": "/face-recognition-app/static/js/main.3cfb028e.chunk.js"
+  },
+  {
+    "revision": "8543a6fc5ab065a3d15a",
+    "url": "/face-recognition-app/static/js/runtime-main.a7be2fa8.js"
+  }
+]);
